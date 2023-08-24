062502f16627300ea4db42d6705cbe4a309c7246
Ali Ehyaie <aliehyaie9469@gmail.com>
2023-08-24 11:41:26 +0330

    feat:create use toggle hook

---
+++ src/hooks/useToggle.tsx
@@ -0,0 +1,12 @@
+import type {Dispatch, SetStateAction} from "react";
+import {useCallback, useState} from "react";
+
+const UseToggle = (defaultValue?: boolean): [boolean, () => void, Dispatch<SetStateAction<boolean>>] => {
+    const [value, setValue] = useState(!!defaultValue);
+
+    const toggle = useCallback(() => setValue(x => !x), []);
+
+    return [value, toggle, setValue];
+};
+
+export default UseToggle;
