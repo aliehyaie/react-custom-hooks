3e1043f6dff346a4234a42114f8bb8d7e1cc43f8
Ali Ehyaie <aliehyaie9469@gmail.com>
2023-09-06 20:24:12 +0330

    feat: create useIntersectionObserver hook

---
+++ src/hooks/useIntersectionObserver.tsx
@@ -0,0 +1,32 @@
+import {RefObject, useEffect, useState} from 'react'
+
+export function useIntersectionObserver(
+    elementRef: RefObject<Element>,
+    freezeOnceVisible = false,
+    options?: IntersectionObserverInit
+): IntersectionObserverEntry | undefined {
+    const {root = null, rootMargin = "0%", threshold = 0} = options || {};
+    const [entry, setEntry] = useState<IntersectionObserverEntry>()
+
+    const frozen = entry?.isIntersecting && freezeOnceVisible
+
+    const updateEntry = ([entry]: IntersectionObserverEntry[]): void => {
+        setEntry(entry)
+    }
+
+    useEffect(() => {
+        const node = elementRef?.current // DOM Ref
+        const hasIOSupport = ('IntersectionObserver' in window)
+
+        if (!hasIOSupport || frozen || !node) return
+
+        const observer = new IntersectionObserver(updateEntry, {threshold, root, rootMargin})
+
+        observer.observe(node)
+
+        return () => observer.disconnect()
+
+    }, [elementRef?.current, root, rootMargin, threshold, frozen])
+
+    return entry
+}
